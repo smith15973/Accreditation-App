8c2d0150c6b80e4c2da46650d8412e49f54fd4dd
smith15973 <smith.15973@osu.edu>
2024-07-10 13:30:49 -0400

    editing seg template done except for changing the associated programs

--- routes/segs.js
+++ routes/segs.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router();
+const mongoose = require('mongoose');
 
 const SegInstruction = require('../models/segInstruction')
 const Plant = require('../models/plant')
@@ -34,4 +35,44 @@ router.route('/instruction/new')
         return res.redirect(`/`);
     }))
 
-    module.exports = router;
+
+router.route('/instruction/:segInstructionID')
+    .get(isLoggedIn, catchAsync(async (req, res) => {
+        const {segInstructionID} = req.params
+        const segInstruction = await SegInstruction.findById(segInstructionID)
+
+        res.render('segs/edit', {segInstruction, fromPlant:req.query.fromPlant});
+    }))
+    .put(isLoggedIn, catchAsync(async(req,res) => {
+        const {segInstructionID} = req.params
+        const segInstruction = await SegInstruction.findById(segInstructionID)
+        segInstruction.set(req.body)
+        let teamLetters;
+        let departmentLetters;
+        if (req.body.team === 'Operations') {
+            teamLetters = 'OP';
+        } else {
+            teamLetters = 'MT';
+        }
+        if (req.body.department === 'Program Administration') {
+            departmentLetters = 'PA';
+        } else {
+            departmentLetters = 'IO';
+        }
+
+        
+
+        segInstruction.segInstructionID = `SEG-${teamLetters}${departmentLetters}-${req.body.segNum}`
+
+        await segInstruction.save()
+        const fromPlant = req.query.fromPlant
+        if (mongoose.isValidObjectId(fromPlant)) {
+            const plant = await Plant.findById(fromPlant)
+            if (plant) {
+                return res.redirect(`/plant/${plant._id}/seg/${segInstructionID}`)
+            }
+        }
+        res.redirect('/')
+    }))
+
+module.exports = router;
