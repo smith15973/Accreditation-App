0c2cd96be4733ff705cc018d498e10e6c8ddfd60
smith15973 <smith.15973@osu.edu>
2024-07-16 15:01:21 -0400

    fixed requestedPlants not being deleted on plant delete

--- models/plant.js
+++ models/plant.js
@@ -35,6 +35,7 @@ const PlantSchema = new Schema({
 PlantSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
         await User.updateMany({ _id: { $in: doc.users } }, { $pull: { plants: doc._id } });
+        await User.updateMany({ requestedPlants: doc._id }, { $pull: { requestedPlants: doc._id } });
         // console.log(await Seg.find({plant: doc._id}))
     }
 });
